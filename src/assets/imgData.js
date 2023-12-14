882109d0848cc1b31e3574296edb4b49cc122453
biglad22 <emmanuelaberuagba22@gmail.com>
2023-12-14 09:32:52 +0100

    hope

--- src/assets/imgData.js
+++ src/assets/imgData.js
@@ -1,76 +1,76 @@
 export const heroBg = [
     {
-      bgPic:'./src/assets/hero-bg/m-at601-pic.jpg',
+      bgPic:'./src/assets/m-at601-pic.jpg',
       priCol:'rgb(254,224,40)',
       opCol:'rgb(127,112,20)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-at602-pic.jpg',
+      bgPic:'./src/assets/m-at602-pic.jpg',
       priCol:'rgb(255,108,76)',
       opCol:'rgb(127,54,38)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-pejuswed-pic.jpg',
+      bgPic:'./src/assets/m-pejuswed-pic.jpg',
       priCol:'rgb(254,228,201)',
       opCol:'rgb(127,114,100)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-wedding2-pic.jpg',
+      bgPic:'./src/assets/m-wedding2-pic.jpg',
       priCol:'rgb(254,228,201)',
       opCol:'rgb(127,114,100)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-wedrecp2-pic.jpg',
+      bgPic:'./src/assets/m-wedrecp2-pic.jpg',
       priCol:'rgb(254,228,201)',
       opCol:'rgb(127,114,100)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withaunt-pic.jpg',
+      bgPic:'./src/assets/m-withaunt-pic.jpg',
       priCol:'rgb(255,235,208)',
       opCol:'rgb(127,117,104)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withema-pic.jpg',
+      bgPic:'./src/assets/m-withema-pic.jpg',
       priCol:'rgb(177,247,255)',
       opCol:'rgb(138,124,127)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withema2-pic.jpg',
+      bgPic:'./src/assets/m-withema2-pic.jpg',
       priCol:'rgb(251,114,122)',
       opCol:'rgb(125,57,61)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withema3-pic.jpg',
+      bgPic:'./src/assets/m-withema3-pic.jpg',
       priCol:'rgb(228,117,149)',
       opCol:'rgb(114,58,74)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withfam-pic.jpg',
+      bgPic:'./src/assets/m-withfam-pic.jpg',
       priCol:'rgb(244,196,120)',
       opCol:'rgb(122,98,60)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withfri-pic.jpg',
+      bgPic:'./src/assets/m-withfri-pic.jpg',
       priCol:'rgb(239,216,198)',
       opCol:'rgb(119,108,99)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withhus-pic.jpg',
+      bgPic:'./src/assets/m-withhus-pic.jpg',
       priCol:'rgb(129,232,251)',
       opCol:'rgb(58,116,129)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withhus5-pic.jpg',
+      bgPic:'./src/assets/m-withhus5-pic.jpg',
       priCol:'rgb(213,247,246)',
       opCol:'rgb(106,123,123)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withpeju-pic.jpg',
+      bgPic:'./src/assets/m-withpeju-pic.jpg',
       priCol:'rgb(129,138,177)',
       opCol:'rgb(64,69,88)'
     },
     {
-      bgPic:'./src/assets/hero-bg/m-withsis2-pic.jpg',
+      bgPic:'./src/assets/m-withsis2-pic.jpg',
       priCol:'rgb(255,177,140)',
       opCol:'rgb(127,88,70)'
     }
